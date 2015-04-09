f485d4295d90a10f86084f40cf1e460e6f86fa7d
Jimmy Lindström <jimmylindstrom79@gmail.com>
2015-04-09 13:49:38 +0200

    Lagt till funktion på sidan för att ändra lösenord

--- includes/modules/byt-losenord.php
+++ includes/modules/byt-losenord.php
@@ -1,3 +1,54 @@
+<?php
+require_once 'core/init.php';
+
+$user = new User();
+
+if(!$user->isLoggedIn()){
+	Redirect::to('index.php');
+}
+
+if(Input::exists()){
+
+	if(Token::check(Input::get('token'))){
+		
+		$validate = new Validate();
+		$validation = $validate->check($_POST, array(
+			'password_current' => array(
+				'required' => true,
+				'min' => 6),
+			'password_new' => array(
+				'required' => true,
+				'min' => 6),
+			'password_new_again' => array(
+				'required' => true,
+				'min' => 6,
+				'matches' => 'password_new')
+		));
+
+		if($validation->passed()){
+			if(Hash::make(Input::get('password_current'), $user->data()->salt) !== $user->data()->password){
+				echo 'Your current password is wrong';
+			}else{
+				$salt = Hash::salt(32);
+				$user->update(array(
+					'password' => Hash::make(Input::get('password_new'), $salt),
+					'salt' =>$salt
+					));
+
+				Session::flash('home', 'Your password has been changed!');
+				Redirect::to('mina-uppgifter.php');
+			}
+
+		}else{
+			foreach($validation->errors() as $error){
+				echo $error, '<br>';
+			}
+		}
+	}
+}
+?>	
+
+
 	<header>
 			<section class="header">
 				<div class="container">
@@ -11,26 +62,27 @@
 										<form action="" method="post" class="form-horizontal">
 											<fieldset>
 												<div class="form-group">
-													<label for="oldpass" class="col-md-2 control-label">Nuvarande lösenord:</label>
+													<label for="password_current" class="col-md-2 control-label">Nuvarande lösenord:</label>
 													<div class="col-md-10">
-														<input type="text" name="fullname" id="oldpass" value="**********" class="form-control">
+														<input type="password" name="password_current" id="password_current" placeholder="**********" autocomplete="off" class="form-control">
 													</div>
 													<div class="form-group">
-													<label for="newpass" class="col-md-2 control-label">Nytt lösenord:</label>
+													<label for="password_new" class="col-md-2 control-label">Nytt lösenord:</label>
 													<div class="col-md-10">
-														<input type="text" name="fullname" id="newpass" value="**********" class="form-control">
+														<input type="password" name="password_new" id="password_new" placeholder="**********" autocomplete="off" class="form-control">
 													</div>
 												</div>
 												<div class="form-group">
-													<label for="repnewpass" class="col-md-2 control-label">Repetera nytt lösenord:</label>
+													<label for="password_new_again" class="col-md-2 control-label">Repetera nytt lösenord:</label>
 													<div class="col-md-10">
-														<input type="email" name="email" id="repnewpass" value="**********" class="form-control">
+														<input type="password" name="password_new_again" id="password_new_again" placeholder="**********" autocomplete="off" class="form-control">
 													</div>
 												</div>
 												
 												<br><br><br>
 												<div class="center">
 													<button type="submit" class="btn btn-success"><span class="fa fa-check"></span> Spara</button>
+													<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
 												</div>
 											</fieldset>
 										</form>
