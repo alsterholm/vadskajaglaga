7a14ada62eeddb70c45cf2901ade5d99146e4cee
Jimmy Lindström <jimmylindstrom79@gmail.com>
2015-04-09 12:21:29 +0200

    Börjat lägga till funktionalitet för att ändra personuppgifter

---
+++ byt-losenord.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Ändra uppgifter - Vad ska jag laga?</title>
+
+		<link rel="icon" type="image/png" href="img/icon.png">
+
+		<!--  Init-file and page protect -->
+		<?php
+		require_once 'core/init.php';
+		protect();
+		?>
+
+		<!-- CSS-links -->
+		<?php include 'includes/data/css-links.php'; ?>	
+	</head>
+
+<?php
+
+	$user = new User();
+	$token = Token::generate();
+
+?>
+
+
+	<body>
+		
+		<!-- Navbar -->
+		<?php include 'includes/navbar.php'; ?>
+
+
+		<!-- Main function -->
+		<?php include 'includes/modules/byt-losenord.php'; ?> 
+
+		
+		<!-- Footer -->
+		<?php include 'includes/footer.php'; ?>
+
+
+		<!-- Modals -->
+			<!-- Login-modal -->
+			<?php include 'modals/login.php'; ?>
+
+			<!-- Register-modal -->
+			<?php include 'modals/register.php'; ?>
+
+
+		<!-- JavaScript-links -->
+		<?php include 'includes/data/js-links.php'; ?>
+
+	</body>
+</html>
--- includes/modules/byt-losenord.php
+++ includes/modules/byt-losenord.php
@@ -1,75 +1,4 @@
-<?php
-require_once 'core/init.php';
-
-$user = new User();
-
-if(!$user->isLoggedIn()){
-	Redirect::to('index.php');
-}
-
-if(Input::exists()){
-	if(Token::check(Input::get('token'))){
-
-		$validate = new Validate();
-		echo Input::get('email');
-		echo $user->data()->email;
-		if(!Input::get('email') ==  $user->data()->email){
-			$validation = $validate->check($_POST, array(
-				'fullname' => array(
-					'required' => true,
-					'min' => 2,
-					'max' => 50,
-					'fullname' => true,
-					'alphabetical' => true
-					),
-				'email' => array(
-					'required' => true,
-					'unique' => 'users',
-					'email' => true
-					)
-
-				));
-		} else {
-			$validation = $validate->check($_POST, array(
-				'fullname' => array(
-					'required' => true,
-					'min' => 2,
-					'max' => 50,
-					'fullname' => true,
-					'alphabetical' => true
-					)
-				));
-
-		}
-		if($validation->passed()){
-				Redirect::to('abc.php');
-
-			try{
-				$user->update(array(
-					'fullname' => Input::get('fullname'),
-					'email' => Input::get('email')
-					));
-
-				Session::flash('home', 'Your details have been updated.');
-				Redirect::to('mina-uppgifter.php');
-
-			}catch(Exception $e){
-				echo 'databasfel';
-				//die($e->getMessage());
-			}
-
-		}else{
-			foreach ($validation->errors() as $error) {
-				echo $error, '<br>';
-			}
-		}
-	}
-
-}
-
-?>
-
-		<header>
+	<header>
 			<section class="header">
 				<div class="container">
 					<div class="row">
@@ -79,7 +8,7 @@ if(Input::exists()){
 								<h1>Ändra uppgifter</h1>
 								<div class="row">
 									<div class="col-md-12">
-										<form action="" method="post" class="form-horizontal">
+										<form action="update-settings.php" method="post" class="form-horizontal">
 											<fieldset>
 												<div class="form-group">
 													<label for="fullname" class="col-md-2 control-label">Namn:</label>
@@ -102,7 +31,6 @@ if(Input::exists()){
 												<br><br><br>
 												<div class="center">
 													<button type="submit" class="btn btn-success"><span class="fa fa-check"></span> Spara</button>
-													<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
 												</div>
 											</fieldset>
 										</form>
