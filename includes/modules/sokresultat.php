15563d9c61adc89c101f479501a4155996628b37
Yassmin Hezarehnia <yassmin.hez@gmail.com>
2015-04-10 12:54:20 +0200

    Sökresultat

---
+++ includes/modules/sokresultat.php
@@ -0,0 +1,24 @@
+	<!-- Search results -->
+		<div class="container">
+			<div class="row">
+				<div class="list-group">
+					<h1> Sökresultat: </h1>
+						<div class="well well-lg main-section">
+							<div class="list-group-item">
+								<div class="row">
+									<div class="col-xs-6 col-md-3">
+										<a href="#" class="thumbnail">
+											<img src="img/pastagratang.jpg" alt="pastagratang">
+										</a>
+									</div>
+								</div>
+								<a href="#">
+								<h4 class="list-group-item-heading"> Pastagratäng med kyckling </h4>
+								</a>
+								<p class="list-group-item-text">Är du sugen på en härlig gratäng? Testa att laga till en riktigt krämig pastagratäng med kyckling. Servera pastagratängen varm tillsammans med en krispig och god sallad. Mättande, matigt och supergott!</p>
+								<h6> <span class="glyphicon glyphicon-time" aria-hidden="true"></span> Tid: Ca 45min</h6>
+							</div>
+						</div>	
+				</div>
+			</div>
+		</div>	
