2a0441356305f0901a31c5509a9fae9912a9c0e0
andreasindahl <andreas@rocketship.se>
2015-04-13 23:20:00 +0200

    Snyggare interaktioner

--- includes/data/js-links.php
+++ includes/data/js-links.php
@@ -2,6 +2,17 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
 		<script src="js/jquery.autocomplete.js"></script>
 		<script src="js/bootstrap.min.js"></script>
-		<script src="js/main.js"></script>
+		<script>
+			$(document).ready(function() {
+				$('.main-section').fadeIn(300);
+			});
 
-		
+			$('a').click(function(e) {
+				var url = $(this).attr('href');
+				$('.main-section').fadeOut(300, function() {
+					window.location.href = url;
+				});
+				e.preventDefault();
+			});
+		</script>
+		<script src="js/main.js"></script>
