520d29df38d4535b3ad811873a25bc089e734d20
andreasindahl <andreas@rocketship.se>
2015-04-14 10:18:06 +0200

    Småfix

--- classes/Mail.php
+++ classes/Mail.php
@@ -12,19 +12,22 @@
  			}
 
  			switch($from) {
- 				case 'newsletter' : $from = 'nyhetsbrev@vadskajaglaga.se';
- 					break;
- 				default : $from = 'info@vadskajaglaga.se';
+ 				case 'nyhetsbrev': $from = 'nyhetsbrev@vadskajaglaga.se';
+ 				break;
+
+ 				case 'kontakt': $from = 'kontakt@vadskajaglaga.se';
+ 				break;
+
+ 				default: $from = 'info@vadskajaglaga.se';
  			}
 
  			// Content-type header set for sending HTML mail
-
 			$headers  = 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 			// Additional headers
 			$headers .= 'To:' . $to_name . ' <' . $to_email . '>' . "\r\n";
-			$headers .= 'From: VadSkaJagLaga <' . $from . '>' . "\r\n";
+			$headers .= 'From: Vad ska jag laga? <' . $from . '>' . "\r\n";
 
 
  			if(!mail($to, $subject, $message, $headers)){
