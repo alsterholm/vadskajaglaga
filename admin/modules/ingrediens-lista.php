9d8d1483c76077dee9f2e85f16c20125ebcecd2e
andreasindahl <andreas@rocketship.se>
2015-04-16 14:43:14 +0200

    Ingrediens-lista

--- admin/modules/ingrediens-lista.php
+++ admin/modules/ingrediens-lista.php
@@ -2,23 +2,44 @@
 <br>
 <div class="row">
 	<div class="col-md-12">
-		<div class="row">
-			<div class="col-md-12">
+		<table class="table table-striped"> 
+			<thead>
+				<tr>
+					<td>ID</td>
+					<td>Namn</td>
+					<td>Textfärg</td>
+					<td>Bakgrundsfärg</td>
+					<td></td>
+				</tr>
+			</thead>
+			<tbody>
 				<?php
-					$ingredients =array();
-					$ingredients = Ingredient::all();
+
+					foreach (Ingredient::all() as $ingredient) {
+
+						switch($ingredient->fgcolor) {
+							case '#ffffff': $fgcolor = 'Vit';
+							break;
+
+							case '#000000': $fgcolor = 'Svart';
+							break;
+						}
+
+						echo '
+							<tr>
+								<td>' . $ingredient->id . '</td>
+								<td>' . $ingredient->name . '</td>
+								<td>' . $fgcolor . '</td>
+								<td>' . strtoupper($ingredient->bgcolor) . '</td>
+								<td style="text-align: right"><a href="#" id="delete' . $ingredient->id . '">Ta bort</a>
+							</tr>
+						';
+					}
+
 				?>
-				<table> 
-					<tr> 
-					    <td>id</td> 
-					    <td>namn</td> 
-					</tr>
-				</table> 
-				
-			</div>
-		</div>
+			</tbody>
+		</table> 
 	</div>
-	
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
