00b91910e53246ccc91790321954948dd726a305
Andreas Indahl <andreas@rocketship.se>
2015-04-29 13:10:08 +0200

    Glömt lösenord-fix

--- includes/modules/aterstall.php
+++ includes/modules/aterstall.php
@@ -60,7 +60,7 @@
 		//Kontrollera att hashen finns i databasen
 		if ($db->count()) {
 			//Kontrollera att det inte har gått mer än 15 minuter sedan mailet skickades.
-			if (time() < ($reset->time + 900)) {
+			if (time() > ($reset->time + 900)) {
 ?>
 
 						<form action="" method="post">
