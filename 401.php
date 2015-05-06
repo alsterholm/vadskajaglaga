20e2c08c9d89d489d5bc0a5a1d70f280b8580e01
Andreas Indahl <andreas@rocketship.se>
2015-05-06 16:58:28 +0200

    Fix i Redirect-klass

---
+++ 401.php
@@ -0,0 +1,43 @@
+<?php header('HTTP/1.1 401 Unauthorized'); ?>
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Obehörig - Vad ska jag laga?</title>
+
+		<link rel="icon" type="image/png" href="img/icon.png">
+
+		<!--  Init-file -->
+		<?php require_once 'core/init.php'; ?>
+
+		<!-- CSS-links -->
+		<?php include 'includes/data/css-links.php'; ?>	
+	</head>
+
+<?php
+
+	$user = new User();
+	$token = Token::generate();
+
+?>
+
+	<body>
+		
+		<!-- Navbar -->
+		<?php include 'includes/navbar.php'; ?>
+
+		<!-- Main function -->
+		<?php include 'includes/errors/401.php'; ?> 
+
+		<!-- Footer -->
+		<?php include 'includes/footer.php'; ?>
+
+		<!-- JavaScript-links -->
+		<?php include 'includes/data/js-links.php'; ?>
+
+	</body>
+</html>
--- classes/Redirect.php
+++ classes/Redirect.php
@@ -1,43 +1,33 @@
 <?php
-
 	/**
 	Klassen hanterar redirects på sidan
-
-
 	*/
 	class Redirect {
 
 		/**
 		Functionen förenklar användandet av redirects.
-
 		Tar emot en parameter som motsvarar vart men vill redirecta($location)
-
 		*/
 		public static function to($location = null) {
 			if ($location) {
 				if (is_numeric($location)) {
-					switch ($location){
-						case 404:
-							header('HTTP/1.0 404 Not Found');
-							include 'includes/errors/404.php';
-							exit();
-						break;
+					switch ($location) {
 						case 401:
-							header('HTTP/1.0 401 Unauthorized');
-							include 'includes/errors/401.php';
+							header('Location: 401.php');
 							exit();
 						break;
 						case 500:
-							header('HTTP/1.0 500 Internal Server Error');
-							include 'includes/errors/500.php';
+							header('Location: 500.php');
 							exit();
-
+						break;
+						default:
+							header('Location: 404.php');
+							exit();
 					}
 				}
-				header('location: ' . $location);
+				header('Location: ' . $location);
 				exit();
 			}
 		}
 	}
-
 ?>
