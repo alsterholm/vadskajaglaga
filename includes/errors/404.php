542fa8ae7d845dfe7ec38ceec10c7904f2618aec
Yassmin Hezarehnia <yassmin.hez@gmail.com>
2015-05-07 11:43:18 +0200

    felsida 404

--- includes/errors/404.php
+++ includes/errors/404.php
@@ -1,1 +1,17 @@
-Oops, that page aint up yet
+		<header>
+			<section class="header">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2"></div>
+						<div class="col-md-8">
+							<div class="well well-lg main-section">
+									<h1>Sidan du letar efter finns inte</h1>
+									<p>	
+										<a href="index.php">Klicka här för att gå tillbaka till startsidan</a>	
+									</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</header>
