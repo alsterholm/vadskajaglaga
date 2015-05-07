f84903edcbdc409399c832ddddeb4922c042ae23
Andreas Indahl <andreas@rocketship.se>
2015-05-07 11:51:56 +0200

    404

--- includes/errors/404.php
+++ includes/errors/404.php
@@ -5,10 +5,14 @@
 						<div class="col-md-2"></div>
 						<div class="col-md-8">
 							<div class="well well-lg main-section">
-									<h1>Sidan du letar efter finns inte</h1>
-									<p>	
-										<a href="index.php">Klicka här för att gå tillbaka till startsidan</a>	
+									<h1>Sidan hittades inte</h1>
+									<p class="center">	
+										Ojdå! Vi kunde inte hitta sidan du letade efter! Det kan bero på att du skrivit in något fel i adressfältet, eller så är det bara vi som klantat oss.
 									</p>
+									<br><br>
+									<div class="center">
+										<a class="btn btn-success"><span class="glyphicon glyphicon-home"></span> Tillbaka till startsidan</a>
+									</div>
 							</div>
 						</div>
 					</div>
